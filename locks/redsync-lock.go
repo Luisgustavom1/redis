828995a01652e884cc6098141dad227894dcb7b7
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-04-14 21:45:28 -0300

    change retries

--- locks/redsync-lock.go
+++ locks/redsync-lock.go
@@ -10,6 +10,7 @@ func read_sync_lock(rs *redsync.Redsync, lockName string, timeout time.Duration)
 	lock_key := LOCK_PREFIX + lockName
 	options := []redsync.Option{
 		redsync.WithExpiry(timeout),
+		redsync.WithTries(3),
 	}
 	mutex := rs.NewMutex(lock_key, options...)
 
